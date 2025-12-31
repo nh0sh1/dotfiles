f698d9448ac64471ae9e397d898a67aff2b3cc0c
nh0sh1 <ishikawa.pc@gmail.com>
2025-12-31 18:53:56 +0900

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,4 +12,15 @@ if vim.g.vscode then
     vim.keymap.set('n', '<Leader>w', '<Cmd>call VSCodeNotify("workbench.action.files.save")<CR>')
     -- 例: スペース + e でエクスプローラー（サイドバー）を開く
     vim.keymap.set('n', '<Leader>e', '<Cmd>call VSCodeNotify("workbench.view.explorer")<CR>')
+
+    vim.opt.undofile = false
+
+    -- VS Code の便利な機能を Vim のキーバインドで呼び出す設定
+    local keymap = vim.keymap.set
+
+    -- 行の移動（VS Code のネイティブ動作を呼び出す）
+    keymap('n', 'gj', "<cmd>call VSCodeNotify('editor.action.moveLinesDownAction')<CR>")
+    keymap('n', 'gk', "<cmd>call VSCodeNotify('editor.action.moveLinesUpAction')<CR>")
+else
+    vim.opt.undofile = true
 end
